1a74b7acaa9a2c74e2a95ee646b13cbe7f8e34f6
streetmapp <streetmapp@gmail.com>
2015-06-26 10:34:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "<h1> you're doing this</h1>
-            <h2> just another test </h2>"
+    return "<h1> you're doing this</h1>"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
