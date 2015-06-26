e8f5e18fb2fa5420d37bfd33d9535da996d854f3
streetmapp <streetmapp@gmail.com>
2015-06-26 10:32:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "<h1> hello OpenShift Ninja</h1>"
+    return "<h1> you're doing this</h1>
+            <h2> just another test </h2>"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
